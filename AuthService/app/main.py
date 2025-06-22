9dd894e1ce040c9e3b505ba4f869d1375bbb08aa
Roman <pom4ekoffical@mail.ru>
2025-06-22 20:14:53 +0500

    changed origins

--- AuthService/app/main.py
+++ AuthService/app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 # CORS если нужен
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
